faecafb61f3798da82890cbca698ccc33ce9a742
Igor Alexandrovich Ivanov <igoralexandrovich95@mail.ru>
2023-01-27 18:56:52 +0500

    Completed

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//
+//3, 5 -> 243 (3⁵)
+//
+//2, 4 -> 16
+
+int Exponentiation(int firstValue, int secondValue) {
+    int count = 0;
+    int result = 1;
+        while(count != secondValue) {
+            result *= firstValue;
+            count++;
+        }
+    System.Console.WriteLine(result);
+    return result;
+}
+
+
