aafd79692b1e2b8e1b411ee5c16c1127da80c4da
Igor Alexandrovich Ivanov <igoralexandrovich95@mail.ru>
2023-01-27 19:34:10 +0500

    Completed

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//
+//452 -> 11
+//
+//82 -> 10
+//
+//9012 -> 12
+
+int CountingDigits(int inputValue) {
+    int result = 0;
+    string parseNumber =  inputValue.ToString();
+    for (int i = 0; i < parseNumber.Length; i++) {
+        result += Convert.ToInt32(Convert.ToString(parseNumber[i]));
+    }
+    
+    System.Console.WriteLine(result);
+    return result;
+}
+
+CountingDigits(1234);
