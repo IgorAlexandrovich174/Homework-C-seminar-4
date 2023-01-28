a00050447f64a4660f2bb0b958e63252c2b28296
Igor Alexandrovich Ivanov <igoralexandrovich95@mail.ru>
2023-01-28 12:42:39 +0500

    Completed

---
+++ task29/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//
+//6, 1, 33 -> [6, 1, 33]
+
+int[] ArrayGeneration() {
+    
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++) {
+        int generationValue = new Random().Next(0,20);
+        array[i] = generationValue;
+    }
+    System.Console.WriteLine(string.Join(", ", array));
+    return array;
+}
+
+ArrayGeneration();
